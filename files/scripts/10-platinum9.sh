f45a685cfc8619526b82940a8f5718319c173972
Adam Fidel <adam@fidel.id>
2025-04-07 10:25:09 -0500

    feat: add platinum9 features

---
+++ files/scripts/10-platinum9.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+cd /tmp
+wget https://github.com/timnetworks/Platinum9/archive/refs/heads/master.zip
+unzip *.zip
+cd Platinum9-master
+
+mv NineIcons /usr/share/icons
+mv Charcoal.ttf MONACO.TTF /usr/share/fonts
+
+cd boot
+unzip System8-master.zip
+cd System8-master
+mv System8 /usr/share/plymouth/themes
+
+plymouth-set-default-theme System8
+
+update-mime-database /usr/share/mime
+gdk-pixbuf-query-loaders-64 --update-cache
+#gtk-update-icon-cache --force --ignore-theme-index /usr/share/icons/NineIcons

f4cf196ef0ce87bd8c1d723aa2645d4d9ee5e790
Adam Fidel <adam@fidel.id>
2025-04-08 00:49:54 -0500

    feat: add vala-panel-appmenu-gtk-module

--- files/scripts/01-main.sh
+++ files/scripts/01-main.sh
@@ -4,4 +4,4 @@ set -exuo pipefail
 
 dnf install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 
-rpm-ostree install -y xfce4-vala-panel-appmenu-plugin
+rpm-ostree install -y xfce4-vala-panel-appmenu-plugin vala-panel-appmenu-gtk-module
